4be31b9e0c05160600d36fd27eec028e6a8be3b5
therealuhlarzoltan <therealuhlarzoltan@gmail.com>
2024-06-16 13:31:55 +0200

    working basic microservices

--- microservices/product-composite-service/src/main/java/hu/therealuhlarzoltan/microservices/composit/product/ProductCompositeServiceApplication.java
+++ microservices/product-composite-service/src/main/java/hu/therealuhlarzoltan/microservices/composit/product/ProductCompositeServiceApplication.java
@@ -2,7 +2,9 @@ package hu.therealuhlarzoltan.microservices.composit.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan("hu.therealuhlarzoltan")
@@ -11,5 +13,9 @@ public class ProductCompositeServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductCompositeServiceApplication.class, args);
 	}
-
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
+
