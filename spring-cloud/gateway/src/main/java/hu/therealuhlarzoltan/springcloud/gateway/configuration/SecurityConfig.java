bb805a1cfa86d41985e25b28941adfd78caac644
therealuhlarzoltan <therealuhlarzoltan@gmail.com>
2024-07-07 19:39:39 +0200

    Authorization fixed

--- spring-cloud/gateway/src/main/java/hu/therealuhlarzoltan/springcloud/gateway/configuration/SecurityConfig.java
+++ spring-cloud/gateway/src/main/java/hu/therealuhlarzoltan/springcloud/gateway/configuration/SecurityConfig.java
@@ -3,6 +3,7 @@ package hu.therealuhlarzoltan.springcloud.gateway.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -13,9 +14,15 @@ public class SecurityConfig {
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges ->
                         exchanges
+                                .pathMatchers("/headerrouting/**").permitAll()
                                 .pathMatchers("/actuator/**").permitAll()
+                                .pathMatchers("/eureka/**").permitAll()
+                                .pathMatchers("/oauth2/**").permitAll()
+                                .pathMatchers("/login/**").permitAll()
+                                .pathMatchers("/error/**").permitAll()
                                 .pathMatchers("/openapi/**").permitAll()
                                 .pathMatchers("/webjars/**").permitAll()
                                 .anyExchange().authenticated()
